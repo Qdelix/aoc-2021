a58162b45986f1bf861b5e1028c8d50d2f6e79ae
Qdelix <dawidkudela.1997@gmail.com>
2021-12-01 23:43:30 +0100

    Second part of day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,5 +2,12 @@ fun main() {
     val input = readInput("Day01")
     val increasedList = input.zipWithNext { a, b -> a.toInt() < b.toInt() }
     val increasedSize = increasedList.filter { it }.size
-    print(increasedSize)
+    println("1st part: $increasedSize")
+
+    val windowedList = input.windowed(3)
+            .zipWithNext { a, b -> getSumOfElementsInList(a) < getSumOfElementsInList(b) }
+    val increasedWindowsSize = windowedList.filter { it }.size
+    println("2nd part: $increasedWindowsSize")
 }
+
+private fun getSumOfElementsInList(list: List<String>) = list.sumOf { it.toInt() }
